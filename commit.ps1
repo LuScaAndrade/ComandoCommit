e5d61b48dbedf254a7bd145c68d9b817c04888f2
LuScaAndrade <Lucasavitorino1@gmail.com>
2025-03-12 14:05:37 -0300

    Arquivo

---
+++ commit.ps1
@@ -0,0 +1,13 @@
+﻿echo "Adicionando arquivos..."
+git add .
+$message = Read-Host "Escreva a mensagem do commit: "
+
+while([string]::IsNullOrWhiteSpace($message)) {
+    echo "Insira uma mensagem!"
+    $message = Read-Host "Escreva a mensagem do commit: "
+}
+
+git commit -m "$message"
+echo "Subindo para o GitHub: "
+git push
+echo "Finalizado!"
